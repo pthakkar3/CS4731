9b619a1dcf3c4403912b8ab9fc0ef738c5b4af35
Thakkar, Pranshav S <pthakkar7@gatech.edu>
2019-05-22 12:13:17 -0400

    uploaded files

---
+++ homework7/src/dk/itu/mario/engine/level/MyDNA.java
@@ -0,0 +1,91 @@
+package dk.itu.mario.engine.level;
+
+import java.util.Random;
+import java.util.*;
+
+//Make any new member variables and functions you deem necessary.
+//Make new constructors if necessary
+//You must implement mutate() and crossover()
+
+
+public class MyDNA extends DNA
+{
+	
+	public int numGenes = 0; //number of genes
+
+	// Return a new DNA that differs from this one in a small way.
+	// Do not change this DNA by side effect; copy it, change the copy, and return the copy.
+	public MyDNA mutate ()
+	{
+		MyDNA copy = new MyDNA();
+		//YOUR CODE GOES BELOW HERE
+		Random rand = new Random();
+		String stockDNA = "abcdefghijklmnopqrstuvwxyz";
+		int mutation = rand.nextInt(stockDNA.length());
+		int mutationLocation = rand.nextInt(this.getChromosome().length());
+		String newChromosome = "";
+		for (int i = 0; i < this.getChromosome().length(); i++) {
+			if (i == mutationLocation) {
+				newChromosome = newChromosome + stockDNA.charAt(mutation);
+			} else {
+				newChromosome = newChromosome + this.getChromosome().charAt(i);
+			}
+		}
+		copy.setChromosome(newChromosome);
+		//YOUR CODE GOES ABOVE HERE
+		return copy;
+	}
+	
+	// Do not change this DNA by side effect
+	public ArrayList<MyDNA> crossover (MyDNA mate)
+	{
+		ArrayList<MyDNA> offspring = new ArrayList<MyDNA>();
+		//YOUR CODE GOES BELOW HERE
+		Random rand = new Random();
+		MyDNA offspring1 = new MyDNA();
+		MyDNA offspring2 = new MyDNA();
+		int crossoverPoint = rand.nextInt(this.getChromosome().length());
+
+		// Crossover as shown in class: abcd + wxyz = abyz + wxcd
+		String chromo1 = this.getChromosome().substring(0, crossoverPoint) + mate.getChromosome().substring(crossoverPoint, mate.getChromosome().length());
+		String chromo2 = mate.getChromosome().substring(0, crossoverPoint) + this.getChromosome().substring(crossoverPoint, this.getChromosome().length());
+
+		offspring1.setChromosome(chromo1);
+		offspring2.setChromosome(chromo2);
+		offspring.add(offspring1);
+		offspring.add(offspring2);
+		//YOUR CODE GOES ABOVE HERE
+		return offspring;
+	}
+	
+	// Optional, modify this function if you use a means of calculating fitness other than using the fitness member variable.
+	// Return 0 if this object has the same fitness as other.
+	// Return -1 if this object has lower fitness than other.
+	// Return +1 if this objet has greater fitness than other.
+	public int compareTo(MyDNA other)
+	{
+		int result = super.compareTo(other);
+		//YOUR CODE GOES BELOW HERE
+		
+		//YOUR CODE GOES ABOVE HERE
+		return result;
+	}
+	
+	
+	// For debugging purposes (optional)
+	public String toString ()
+	{
+		String s = super.toString();
+		//YOUR CODE GOES BELOW HERE
+		
+		//YOUR CODE GOES ABOVE HERE
+		return s;
+	}
+	
+	public void setNumGenes (int n)
+	{
+		this.numGenes = n;
+	}
+
+}
+
